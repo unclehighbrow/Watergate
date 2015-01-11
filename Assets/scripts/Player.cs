1f14adf46dd59fce79826b9f9c7532a1b38d83d2
Will Carlough <will@carlough.net>
2015-01-10 22:56:15 -0500

    pellets

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "pellet") {
+			Destroy(col.gameObject);
+		} else if (col.gameObject.tag == "power_pellet") {
+			Destroy(col.gameObject);
+		}
+
+	}
+}
