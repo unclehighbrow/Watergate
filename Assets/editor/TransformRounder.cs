34fb5a65b5299b5b390b029dd239303d4485a48f
Will Carlough <will@carlough.net>
2015-03-08 14:18:38 -0400

    putting acorn files under source control

---
+++ Assets/editor/TransformRounder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TransformRounder : MonoBehaviour {
+
+	[MenuItem("GameObject/Round Transform")]
+	public static void RoundTransform() {
+		GameObject[] gos = Selection.gameObjects;
+		foreach (GameObject go in gos) {
+			Vector3 position = go.transform.position;
+			position.x = Mathf.Round(position.x * 2) / 2;
+			position.y = Mathf.Round(position.y * 2) / 2;
+			position.z = 0;
+			go.transform.position = position;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
